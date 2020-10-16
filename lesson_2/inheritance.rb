8874232a6d8f4317e96e32bb9b6e2daf849d5737
Josh Leath <lawnfurniture@icloud.com>
2020-10-15 21:57:08 -0400

    completed inheritance lecture

---
+++ lesson_2/inheritance.rb
@@ -0,0 +1,50 @@
+class Pet
+  def run
+    'running!'
+  end
+
+  def jump
+    'jumping!'
+  end
+end
+
+class Dog < Pet
+  def speak
+    'bark!'
+  end
+
+  def fetch
+    'fetching!'
+  end
+
+  def swim
+    'swimming!'
+  end
+end
+
+class Cat < Pet
+  def speak
+    'meow!'
+  end
+end
+
+class Bulldog < Dog
+  def swim
+    "can't swim!"
+  end
+end
+
+pete = Pet.new
+kitty = Cat.new
+dave = Dog.new
+bud = Bulldog.new
+
+puts pete.run
+
+puts kitty.run
+puts kitty.speak
+
+puts dave.speak
+
+puts bud.run
+puts bud.swim
