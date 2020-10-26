864286fc024e4184941b45f57b2367a11a3a1be5
Josh Leath <lawnfurniture@icloud.com>
2020-10-26 08:48:57 -0400

    added clear_screen_and_display_board

--- lesson_5/tictactoe/tictactoe.rb
+++ lesson_5/tictactoe/tictactoe.rb
@@ -97,8 +97,7 @@ class TTTGame
     system('clear') || system('cls')
   end
 
-  def display_board(clear = true)
-    clear_screen if clear
+  def display_board()
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -113,6 +112,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts "     |     |"
     puts ""
+  end
+
+  def clear_screen_and_display_board
+    clear_screen
+    display_board
   end
 
   def human_moves
@@ -164,7 +168,7 @@ class TTTGame
     display_welcome_message
 
     loop do
-      display_board(clear: false)
+      clear_screen_and_display_board
 
       loop do
         human_moves
